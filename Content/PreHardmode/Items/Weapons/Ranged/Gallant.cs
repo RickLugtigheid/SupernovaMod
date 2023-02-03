5f354ecf3564a7a84aac4ec412aa5005e0e03318
Rick Lugtigheid <rlugtigheid77@gmail.com>
2023-02-03 16:55:14 +0100

    Reworked Gallant

--- Content/PreHardmode/Items/Weapons/Ranged/Gallant.cs
+++ Content/PreHardmode/Items/Weapons/Ranged/Gallant.cs
@@ -1,15 +1,22 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Supernova.Content.PreHardmode.Items.Weapons.BaseWeapons;
+using System.IO;
 using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
 using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace Supernova.Content.PreHardmode.Items.Weapons.Ranged
 {
     public class Gallant : SupernovaGunItem
     {
-        int _shots = 0;
+        private const int MAX_STORED_SHOTS = 6;
+        private int _storedShots = MAX_STORED_SHOTS;
+
         public override void SetStaticDefaults()
         {
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
@@ -18,55 +25,141 @@ namespace Supernova.Content.PreHardmode.Items.Weapons.Ranged
             Tooltip.SetDefault("Can shoot 6 bullets before having to cooldown.");
         }
 
-        public override Vector2? HoldoutOffset() => new Vector2(-2, 0);
-
         public override void SetDefaults()
         {
 			base.SetDefaults();
 
-			Item.damage = 10;
-            Item.width = 40;
-            Item.crit = 3;
-            Item.height = 20;
-            Item.noMelee = true; //so the item's animation doesn't do damage
-            Item.knockBack = 0.7f;
-            Item.value = Item.buyPrice(0, 4, 30, 0);
-            Item.autoReuse = false;
-            Item.rare = ItemRarityID.Green;
-            Item.UseSound = SoundID.Item38;
-            Item.shootSpeed = 8;
-            Item.useAmmo = AmmoID.Bullet;
+			Item.damage = 17;
+			Item.width = 50;
+			Item.height = 24;
+			Item.crit = 3;
+			Item.useTime = 10;
+			Item.useAnimation = 10;
+			Item.knockBack = 3;
+			Item.value = Item.buyPrice(0, 4, 30, 0);
+			Item.autoReuse = false;
+			Item.rare = ItemRarityID.Green;
+			Item.UseSound = SoundID.Item41;
+			Item.shootSpeed = 8;
 
-            Item.useAnimation = 4;
-            Item.useTime = 2;
-            Item.reuseDelay = 2;
+			Item.useAmmo = AmmoID.None;
 
-            Item.scale = .8f;
-        }
-        public override bool CanUseItem(Player player)
+			Item.scale = .75f;
+
+			Gun.spread = 2;
+		}
+
+		public override bool CanUseItem(Player player)
         {
-            // Check if our Gallant is cooling down
-            if (player.HasBuff(ModContent.BuffType<Global.Buffs.GallantCooldown>()))
-                return false;
+			// Check if our Gallant is cooling down
+			if (player.HasBuff(ModContent.BuffType<Global.Buffs.GallantCooldown>()))
+			{
+				return false;
+			}
 
-            // After 6 shots the player will get the GallantCooldown debuff
-            _shots++;
-            if (_shots >= 3)
-            {
-                player.AddBuff(ModContent.BuffType<Global.Buffs.GallantCooldown>(), Item.useTime * 30); // The use time will be the ammount of seconds needed for cooldown
-                _shots = 0;
-            }
+			// We can not shoot and reload at the same time
+			if (player.altFunctionUse == ItemAlternativeFunctionID.ActivatedAndUsed)
+			{
+				// Dont reload when we have 6 rounds loaded
+				if (_storedShots != MAX_STORED_SHOTS)
+				{
+					Reload(player);
+				}
+				return false;
+			}
+
             return base.CanUseItem(player);
         }
-        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
-        {
-            // Add random spread to our projectile
-            velocity = velocity.RotatedByRandom(MathHelper.ToRadians(2));
 
-            base.ModifyShootStats(player, ref position, ref velocity, ref type, ref damage, ref knockback);
-        }
+		public override bool? UseItem(Player player)
+		{
+			_storedShots--;
 
-        public override void AddRecipes()
+			// After 6 shots the player will have to reload
+			//
+			if (_storedShots <= 0)
+			{
+				Reload(player);
+			}
+
+			return base.UseItem(player);
+		}
+
+		public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+		{
+			Texture2D bulletTexture = TextureAssets.Item[ItemID.MusketBall].Value;
+
+			// Draw stored bullets to let the player know how many are left
+			//
+			for (int i = 0; i < _storedShots; i++)
+			{
+				spriteBatch.Draw(bulletTexture,
+					position: new Vector2(position.X + (bulletTexture.Width/2 * i), position.Y + bulletTexture.Height * 1.2f),
+					sourceRectangle: new Rectangle(0, 0, bulletTexture.Width, bulletTexture.Height),
+					Color.White,
+					rotation: 0f,
+					origin: Vector2.Zero,
+					scale: new Vector2(.5f, .5f),
+					SpriteEffects.None,
+					layerDepth: 1
+				);
+			}
+		}
+
+		/// <summary>
+		/// We can not shoot when reloading
+		/// </summary>
+		/// <param name="player"></param>
+		/// <returns></returns>
+		public override bool CanShoot(Player player) => player.altFunctionUse == ItemAlternativeFunctionID.None;
+		
+		public override bool AltFunctionUse(Player player) => true;
+		/// <summary>
+		/// Handles our reload
+		/// </summary>
+		/// <param name="player"></param>
+		private void Reload(Player player)
+		{
+			SoundEngine.PlaySound(SoundID.Item149);
+			player.AddBuff(ModContent.BuffType<Global.Buffs.GallantCooldown>(), Item.useTime * 10); // The use time will be the ammount of seconds needed for cooldown
+
+			Item.useAmmo = AmmoID.Bullet;
+
+			// Consume ammo and increase _storedShots until fully filled or no ammo left
+			//
+			for (; _storedShots < MAX_STORED_SHOTS; _storedShots++)
+			{
+				// Try to get ammo
+				Item? ammo = player.ChooseAmmo(Item);
+				if (ammo == null)
+				{
+					break;
+				}
+				player.ConsumeItem(ammo.type);
+			}
+			Item.useAmmo = AmmoID.None;
+		}
+
+		#region Save stored ammo
+		public override void SaveData(TagCompound tag)
+		{
+			tag["ammoStored"] = _storedShots;
+		}
+		public override void LoadData(TagCompound tag)
+		{
+			_storedShots = tag.GetInt("ammoStored");
+		}
+		public override void NetSend(BinaryWriter writer)
+		{
+			writer.Write(_storedShots);
+		}
+		public override void NetReceive(BinaryReader reader)
+		{
+			_storedShots = reader.ReadInt32();
+		}
+		#endregion
+
+		public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe();
             recipe.AddIngredient(ItemID.GoldBar, 7);
